7183c11a57b02c16a66c993636411782c6b618fd
Julien HENRY <julien.henry@sonarsource.com>
2020-05-12 00:12:04 +0200

    More tests

--- its/src/test/java/org/sonarlint/intellij/its/SayHelloJavaTest.java
+++ its/src/test/java/org/sonarlint/intellij/its/SayHelloJavaTest.java
@@ -1,6 +1,8 @@
 package org.sonarlint.intellij.its;
 
 import com.intellij.remoterobot.RemoteRobot;
+import java.time.Duration;
+import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.sonarlint.intellij.its.pages.WelcomeFrame;
@@ -16,7 +18,11 @@ public class SayHelloJavaTest {
 
     @Test
     void checkSayHello() {
-        final RemoteRobot remoteRobot = new RemoteRobot("http://127.0.0.1:8082");
+        final RemoteRobot remoteRobot = new RemoteRobot("http://127.0.0.1:8082",
+          new OkHttpClient.Builder()
+            .readTimeout(Duration.ofMinutes(10))
+            .connectTimeout(Duration.ofMinutes(10))
+            .build());
         final WelcomeFrame welcomeFrame = remoteRobot.find(WelcomeFrame.class);
         assert (welcomeFrame.hasText(startsWith("Version 20")));
     }
